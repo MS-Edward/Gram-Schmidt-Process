1bf7e1d7f9c522fb6b3697e009c1528b808979f1
Ruben Lopez <atf.edward@gmail.com>
2017-07-15 13:12:00 -0500

    Added a Reader class
    Reader Class will retrieve user input in a String format and proceed to
    parse it into a usable vector.

--- src/GramSchmidt.java
+++ src/GramSchmidt.java
@@ -3,14 +3,20 @@
  */
 import java.util.HashMap;
 import java.util.Map;
+import java.util.*;
 
 
 public class GramSchmidt {
 
-
+    //vectorMap will dynamically store our vectors.
+    HashMap<String,Vector> vectorMap = new HashMap<String,Vector>();
 
     public static void main(String[] args)
     {
-
+        Reader inputReader = new Reader();
+        Vector theVector = inputReader.getVector();
+        System.out.print(theVector.toString());
     }
+
+
 }
---
+++ src/Reader.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Vector;
+
+/**
+ * Created by CN_Ed on 7/15/2017.
+ */
+public class Reader {
+
+    public Vector getVector()
+    {
+        System.out.print("Specify vector: ");
+        Scanner sc = new Scanner(System.in);
+        String vector = sc.nextLine();
+
+        return parseVector(vector);
+    }
+
+    private Vector parseVector(String vector)
+    {
+        Scanner sc = new Scanner(vector);
+        Vector theVector = new Vector();
+
+        while(sc.hasNext())
+        {
+            theVector.addElement(sc.next());
+        }
+
+        return theVector;
+    }
+}
